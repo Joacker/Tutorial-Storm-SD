96af30c7907b6b0ca016cef24f518cab7162350c
Joako-Sador <jfernandezvizcarra@gmail.com>
2022-05-08 11:43:44 -0400

    almost fixed

--- topology-deployment-scripts/deploy-topology.sh
+++ topology-deployment-scripts/deploy-topology.sh
@@ -2,4 +2,4 @@
 echo "Waiting for storm nimbus..."
 /topology-deployment-scripts/wait-for.sh nimbus:6627 -- echo "Nimbus is up"
 
-/apache-storm-*/bin/storm jar /topology/storm-word-count.jar io.koszolko.storm.example.WordCountTopology word-count-topology
+/apache-storm-2.2.0/bin/storm jar /topology/storm-word-count.jar io.koszolko.storm.example.WordCountTopology word-count-topology
